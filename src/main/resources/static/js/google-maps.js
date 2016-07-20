6e6adf8547a417ddfd76cb6b1d0f94737c178b82
Philc90 <philipchiang5@gmail.com>
2016-07-20 16:39:19 -0700

    fixed bug with google maps not loading correctly in modal. Refactoring. Added Eventbrite functionality as well as info window.

--- src/main/resources/static/js/google-maps.js
+++ src/main/resources/static/js/google-maps.js
@@ -5,33 +5,30 @@
 
 // map markers and info windows
 var markers = [],
- infoWindows = [], 
- // array of Business objects, obtained from yelp api, used for displaying store info in a table
- businesses = [], 
- // google map
- map, 
- // visitor's current location
- userLat, userLng, 
- // labels for map markers, each one letter
- labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
- // index of next available label, start from 0 
- labelIndex = 0,
- // array of venues
- venues = [];
-
-// google.maps.event.addListenerOnce(map, 'idle', function() {
-//    google.maps.event.trigger(map, 'resize');
-// });
-// callback function from google maps
-function mapsCallback() {
-  getLocation();
-}
+    infoWindows = [], 
+    // array of Business objects, obtained from yelp api, used for displaying store info in a table
+    businesses = [], 
+    // google map
+    map, 
+    // visitor's current location
+    // default to los angeles
+    userLat = 34.0522342,
+    userLng = -118.2436849,
+    // have asked user to accept geolocation?
+    haveAskedUserLoc = false;
+    // labels for map markers, each one letter
+    labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    // index of next available label, start from 0 
+    labelIndex = 0,
+    // array of venues
+    venues = [],
+    events = [];
 
 // uses HTML5 geolocation to get the user's locaton
 //  The navigator object contains information about the browser.
 function getLocation() {
     if (navigator.geolocation) {
-      // if able to get location calls successGeolocation function, else calls errorGeolocation
+        // if able to get location calls successGeolocation function, else calls errorGeolocation
         navigator.geolocation.getCurrentPosition(successGeolocation, errorGeolocation);
     } 
     else { 
@@ -43,31 +40,33 @@ function getLocation() {
 // if successfully got user location then initialize map on user location
 function successGeolocation(position)
 {
-  // position.coords is the user location from HTML5 geolocation api
-  userLat = position.coords.latitude;
-  userLng = position.coords.longitude;
-  initMap();
+    // position.coords is the user location returned from HTML5 geolocation api
+    userLat = position.coords.latitude;
+    userLng = position.coords.longitude;
+    updateMap();
 }
 
 function errorGeolocation(error)
 {
-  alert('ERROR(' + error.code + '): ' + error.message);
-  // default to Los Angeles
-  userLat = 34.0522342;
-  userLng = -118.2436849;
-  initMap();
+    alert('ERROR(' + error.code + '): ' + error.message);
+    // default to Los Angeles if user declines geolocation or error happened
 }
 
-// initializes map based on whether or not geoloction worked
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'));
-  map.setCenter(new google.maps.LatLng(userLat, userLng));
-  map.setZoom(13);
-  map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
+// helper function for getLocation()
+function updateMap() {
+    map.setCenter(new google.maps.LatLng(userLat, userLng));
+}
 
-//   google.maps.event.addListenerOnce(map, 'idle', function() {
-//    google.maps.event.trigger(map, 'resize');
-// });
+// initializes google map
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'));
+    map.setCenter(new google.maps.LatLng(userLat, userLng));
+    map.setZoom(13);
+    map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
+
+    google.maps.event.addListenerOnce(map, 'idle', function() {
+        google.maps.event.trigger(map, 'resize');
+    });
 }
 
 // ajax calls
@@ -79,12 +78,12 @@ $("#yelp").click(function() {
         type:'GET',
         success: function(jsonResp) {
             updateMapYelp(jsonResp);
-        }               
+        }
     });
 });
 
 $("#eventbrite").click(function() {
-  $.ajax({
+    $.ajax({
         url: "/events/" + userLat + "/" + userLng + "/",
         type:'GET',
         success: function(jsonResp) {
@@ -95,140 +94,162 @@ $("#eventbrite").click(function() {
 
 // test of google search
 $("#google").click(function() {
-  $.ajax({
+    $.ajax({
         url: "/testjson",
         type:'GET',
         success: function(jsonResp) {
             updateMapGoogle(jsonResp);
         }
-  });
+    });
 });
+
+// show the info of each business in #map-modal
+function displayYelpInfo() {
+    hideElemById("#eventbrite-info");
+    showElemById("#yelp-info");
+    hideElemById("#eventbrite");
+    showElemById("#yelp");
+}
+
+// show the info of each event in #map-modal
+function displayEventbriteInfo() {
+    hideElemById("#yelp-info");
+    showElemById("#eventbrite-info");
+    hideElemById("#yelp");
+    showElemById("#eventbrite");
+}
+
+// helper function for display...Info
+function showElemById(id) {
+    $(id).removeClass("hidden");
+}
+
+// helper function for display...Info
+function hideElemById(id) {
+    $(id).addClass("hidden");
+}
 
 // populates map with markers from arbitrary test
 function updateMapGoogle(input) {
-  var myjson = jQuery.parseJSON(input);
-  deleteMarkers();
-  addMarkersAndInfoGoogle(myjson);
-  fitBoundsToMarkers();
+    var myjson = jQuery.parseJSON(input);
+    deleteMarkers();
+    addMarkersAndInfoGoogle(myjson);
+    fitBoundsToMarkers();
 }
 
 // helper function for updateMapGoogle
 function addMarkersAndInfoGoogle(json) {
     if(json.status == "OK") {
-      map.setCenter(new google.maps.LatLng(34.0522342, -118.2436849));
-      map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
+        map.setCenter(new google.maps.LatLng(34.0522342, -118.2436849));
+        map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
 
-      json.results.forEach(function(place){
+        json.results.forEach(function(place){
 
-        // Create a marker for each place.
-        var marker = makeMarker(map, place.name, place.geometry.location);
+            // Create a marker for each place.
+            var marker = makeMarker(map, place.name, place.geometry.location);
 
-        // Create an infoWindow with the place's name for each place
-        var infoWindow = makeInfoWindow(place.name);
+            // Create an infoWindow with the place's name for each place
+            var infoWindow = makeInfoWindow(place.name);
 
-        // Want to show only one infoWindow at a time
-        addListenersToMarker(marker, infoWindow);
+            // Want to show only one infoWindow at a time
+            addListenersToMarker(marker, infoWindow);
 
-        // Push each marker & infoWindow into arrays
-        markers.push(marker);
-        infoWindows.push(infoWindow);
-      });
+            // Push each marker & infoWindow into arrays
+            markers.push(marker);
+            infoWindows.push(infoWindow);
+        });
 
 
-      }
-  }
+    }
+}
 
 // populates map with markers from around the user's location
 function updateMapYelp(input) {
-  var myjson = jQuery.parseJSON(input);
-  deleteMarkers();
-  labelIndex = 0;
-  businesses.length = 0;
-  addMarkersAndInfoYelp(myjson);
-  fitBoundsToMarkers();
+    var myjson = jQuery.parseJSON(input);
+    deleteMarkers();
+    labelIndex = 0;
+    businesses.length = 0;
+    addMarkersAndInfoYelp(myjson);
+    // TODO: fitBoundsToMarkers fits bounds to middle of the ocean only for eventbrite
+    // fitBoundsToMarkers();
 }
 
 // helper function for updateMapYelp
 function addMarkersAndInfoYelp(json){
-  map.setCenter(new google.maps.LatLng(json.region.center.latitude, json.region.center.longitude));
+    map.setCenter(new google.maps.LatLng(json.region.center.latitude, json.region.center.longitude));
 
-  json.businesses.forEach(function(place){
-      var marker = makeMarker(map, place.name, 
+    json.businesses.forEach(function(place){
+        var marker = makeMarker(map, place.name, 
         new google.maps.LatLng({
             lat: place.location.coordinate.latitude,
             lng: place.location.coordinate.longitude
         }));
-      var infoWindow = makeInfoWindow(place.name);
-      var business = new Business(marker.label, place.name, place.rating, place.is_closed, place.location.display_address, place.display_phone);
-      // listener from corresponding infoWindow to marker
-      addListenersToMarker(marker, infoWindow);
-      markers.push(marker);
-      infoWindows.push(infoWindow);
-      businesses.push(business);
-  });
+        var infoWindow = makeInfoWindow(place.name);
+        var business = new Business(marker.label, place.name, place.rating, place.is_closed, place.location.display_address, place.display_phone);
+        // listener from corresponding infoWindow to marker
+        addListenersToMarker(marker, infoWindow);
+        markers.push(marker);
+        infoWindows.push(infoWindow);
+        businesses.push(business);
+    });
 }
 
 function updateMapEventbrite(input) {
-  var myjson = jQuery.parseJSON(input);
-  deleteMarkers();
-  labelIndex = 0;
-  venues.length = 0;
-  addMarkersAndInfoEventbrite(myjson);
-  // fitBoundsToMarkers();
-// map.setCenter(new google.maps.LatLng(userLat, userLng));
-//   map.setZoom(13);
+    var myjson = jQuery.parseJSON(input);
+    deleteMarkers();
+    labelIndex = 0;
+    venues.length = 0;
+    addMarkersAndInfoEventbrite(myjson);
+    fitBoundsToMarkers();
+    // map.setCenter(new google.maps.LatLng(userLat, userLng));
+    //   map.setZoom(13);
 }
 
 // helper function for updateMapEventbrite
 function addMarkersAndInfoEventbrite(json) {
+    var latLngBounds = new google.maps.LatLngBounds();
+    var maxNumOfEventsIWant = 10;
+    var counter = 0;
+    json.events.forEach(function(listing) {
+        // each event takes place at a venue
+        // var venue = getVenue(listing.venue_id);
+        var deferredObj = getVenue(listing.venue_id);
+        // console.log("got a ven: " + venues[0]);
+        // console.log("got a venue: " + venue.address);
+        // .then() ensures that the function inside is called after the venue info is loaded
+        deferredObj.then(function() {
+            // console.log("listing name: " + listing.name.text);
+            if(counter < maxNumOfEventsIWant) {
+                var lastAdded = venues[venues.length - 1];
+                var marker = makeMarker(map, listing.name.text, 
+                    new google.maps.LatLng({
+                        lat: lastAdded.latitude,
+                        lng: lastAdded.longitude
+                    })
+                );
+                events.push(new Event(marker.label, listing.name.text, lastAdded.address + ", " + lastAdded.city + ", "+ lastAdded.region + " " + String(lastAdded.postal_code), listing.url));
+                // console.log(events[counter]);
+                var infoWindow = makeInfoWindow(listing.name.text);
+                // listener from corresponding infoWindow to marker
+                addListenersToMarker(marker, infoWindow);
+                markers.push(marker);
+                // latLngBounds.extend(marker.getPosition());
+                // console.log("extend " + marker.getPosition().lat() + ", " + marker.getPosition().lng());
+                // console.log("markers: " + markers);
+                infoWindows.push(infoWindow);
+            }
+            counter++;
+        });
+        // fitBoundsToMarkers();
+    });
+    // map.fitBounds(latLngBounds);
 
-  var maxNumOfEventsIWant = 10;
-  var counter=0;
-  json.events.forEach(function(listing) {
-      // each event takes place at a venue
-      // var venue = getVenue(listing.venue_id);
-      var deferredObj = getVenue(listing.venue_id);
-      // console.log("got a ven: " + venues[0]);
-      // console.log("got a venue: " + venue.address);
-      // .then() ensures that the function inside is called after the venue info is loaded
-      deferredObj.then(function() {
-          console.log("listing name: " + listing.name.text);
-          if(counter < maxNumOfEventsIWant) {
-              var lastAdded = venues[venues.length - 1];
-              var marker = makeMarker(map, listing.name.text, 
-              new google.maps.LatLng({
-                  lat: lastAdded.latitude,
-                  lng: lastAdded.longitude
-              }));
-
-              var infoWindow = makeInfoWindow(listing.name.text);
-              // listener from corresponding infoWindow to marker
-              addListenersToMarker(marker, infoWindow);
-              markers.push(marker);
-              infoWindows.push(infoWindow);
-          }
-          counter++;
-      });
-  });
-
-  
-  
 }
-
-// function test() {
-//   var venue;
-//   $.getJSON("test.json", function(json) {
-//     // venue = JSON.parse(json);
-//     alert(json.address.city);
-//   });
-
-// }
-// test();
 
 // $.ajax returns a Deferred object
 function getVenue(venue_id) {
-  var venue;
-  return $.ajax({
+    var venue;
+    return $.ajax({
         url: "/getVenue/" + venue_id + "/",
         type:'GET',
         dataType: 'json',
@@ -236,15 +257,15 @@ function getVenue(venue_id) {
             // var ven = jQuery.parseJSON(jsonResp);
             // console.log(ven.address);
             // console.log("jsonResp: " + jsonResp.address);
-            venue=new Venue(jsonResp.address.address_1, jsonResp.address.city, jsonResp.address.state, jsonResp.address.postal_code, jsonResp.name, jsonResp.latitude, jsonResp.longitude);
+            venue = new Venue(jsonResp.address.address_1, jsonResp.address.city, jsonResp.address.state, jsonResp.address.postal_code, jsonResp.name, jsonResp.latitude, jsonResp.longitude);
             venues.push(venue);
             // console.log(jsonResp.address.address_1+","+jsonResp.address.city+","+ jsonResp.address.state+","+ 
             //   jsonResp.address.postal_code+","+ jsonResp.name+","+jsonResp.latitude+","+ jsonResp.longitude);
-            
+
         }
-  });
-  // console.log("ven: " + ven.address.city);
-  // return ven;
+    });
+    // console.log("ven: " + ven.address.city);
+    // return ven;
 }
 
 // helper function for initializing maps
@@ -252,55 +273,56 @@ function getVenue(venue_id) {
 // title: the title of the marker (appears as a tooltip when hovering over marker)
 // position: a google.maps.LatLng object
 function makeMarker(map, title, position) {
-  return new google.maps.Marker({
-    map: map,
-    title: title,
-    position: position,
-    // labels each marker with a different letter of the alphabet
-    label: labels[labelIndex++ % labels.length]
-  });
+    return new google.maps.Marker({
+        map: map,
+        title: title,
+        position: position,
+        // labels each marker with a different letter of the alphabet
+        label: labels[labelIndex++ % labels.length]
+    });
 }
 
 // helper function for initializing maps
 // makes the info window for each marker
 function makeInfoWindow(content) {
-  return new google.maps.InfoWindow({
-      content: content
-  });
+    return new google.maps.InfoWindow({
+        content: content
+    });
 }
 
 // helper function for initializing maps
 // adds click listener to each marker to open an info window, also makes
 // sure only one info window open at one time
 function addListenersToMarker(marker, infoWindow) {
-  marker.addListener('click', function() {
-    infoWindow.open(map, marker);
-    // Don't want more than one window open at a time
-    // TODO: better function for closing the other windows
-    infoWindows.forEach(function(item){
-      if(item.content!=infoWindow.content)
-        item.close();
+    marker.addListener('click', function() {
+        infoWindow.open(map, marker);
+        // Don't want more than one window open at a time
+        // TODO: better function for closing the other windows
+        infoWindows.forEach(function(item){
+        if(item.content!=infoWindow.content)
+            item.close();
+        });
     });
-  });
 }
 
 // helper function for initializing maps
 // extend the map bounds so that all the markers are shown
 function fitBoundsToMarkers(){
-  var latLngBounds = new google.maps.LatLngBounds();
-  markers.forEach(function(marker) {
-    latLngBounds.extend(marker.getPosition());
-  });
-  map.fitBounds(latLngBounds);
+    var latLngBounds = new google.maps.LatLngBounds();
+    markers.forEach(function(marker) {
+        console.log("trying to fit " + marker.lat() + "," + marker.lng());
+        latLngBounds.extend(marker.getPosition());
+    });
+    map.fitBounds(latLngBounds);
 }
 
 // helper function for initializing maps
 // removes all the markers from the map
 function deleteMarkers() {
     for(var i=0; i<markers.length; i++) {
-      markers[i].setMap(null);
+        markers[i].setMap(null);
     }
-    markers = [];
+    markers.length = 0;
 }
 
 // makes sure that when the map is opened in the modal window it refreshes
@@ -311,41 +333,63 @@ function deleteMarkers() {
 // });
 
 function Business(index, name, rating, is_closed, address, phone) {
-  this.index = index;
-  this.name = name;
-  this.rating = rating;
-  if(is_closed == false)
-    this.open = "Open";
-  else
-    this.open = "Closed";
-  this.address = address[0] + " " + address[2];
-  this.phone = phone;
-  console.log("made a biz, name: "+this.name+", rating: " + this.rating);
+    this.index = index;
+    this.name = name;
+    this.rating = rating;
+    if(is_closed == false)
+        this.open = "Open";
+    else
+        this.open = "Closed";
+    this.address = address[0] + " " + address[2];
+    this.phone = phone;
+    // console.log("made a biz, name: "+this.name+", rating: " + this.rating);
 }
 
 function Venue(address_1, city, state, postal_code, name, latitude, longitude) {
-  this.address = address_1;
-  this.city = city;
-  this.region = state;
-  this.postal_code = parseFloat(postal_code);
-  this.name = name;
-  this.latitude = parseFloat(latitude);
-  this.longitude = parseFloat(longitude);
-  console.log("made a ven, lat:"+latitude+", long: "+longitude);
+    this.address = address_1;
+    this.city = city;
+    this.region = state;
+    this.postal_code = parseFloat(postal_code);
+    this.name = name;
+    this.latitude = parseFloat(latitude);
+    this.longitude = parseFloat(longitude);
+    // console.log("made a ven, lat:"+latitude+", long: "+longitude);
+}
+
+function Event(index, name, address, link) {
+    this.index = index;
+    this.name = name;
+    this.address = address;
+    this.link = link;
 }
   
-angular.module("yelpApp", []).controller("YelpCtrl", function($scope) {
-  $scope.getBusinesses = function() {
-    // google.maps.event.addListenerOnce(map, 'idle', function(){
-    $scope.businesses = businesses;
-    console.log("in yelpctrl" + $scope.businesses);
-    // });
-    
-  }
+var myApp = angular.module("myApp", [])
+
+myApp.controller("YelpCtrl", function($scope) {
+    // for #yelp-info
+    $scope.getBusinesses = function() {
+        // google.maps.event.addListenerOnce(map, 'idle', function(){
+        $scope.businesses = businesses;
+        console.log("in yelpctrl" + $scope.businesses);
+        // });
+    }
 });
 
-// function test() {
-//   google.maps.event.addListenerOnce(map, 'idle', function() {
-//    google.maps.event.trigger(map, 'resize');
-// });
-// }
+myApp.controller("EventbriteCtrl", function($scope) {
+    // for #eventbrite-info
+    $scope.getEvents = function() {
+        $scope.events = events;
+    }
+});
+
+$(".open-map-modal").click(function() {
+    // once the modal window opens up, then load the google map (otherwise it won't load correctly)
+    $("#map-modal").on("show.bs.modal", function(event) {
+        if(!haveAskedUserLoc) {
+            getLocation();
+            initMap();
+            haveAskedUserLoc = true;
+        }
+    });
+    $("#map-modal").modal("show");
+});
