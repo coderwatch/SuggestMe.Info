c12a99c882710df27d4e2c1cd8fb1dbedd3526ee
Oscar Nevarez <ironbeaner@gmail.com>
2016-07-21 03:12:48 -0700

    need to fix the image problem using ajax

--- src/main/resources/static/js/book.js
+++ src/main/resources/static/js/book.js
@@ -1,65 +1,66 @@
 
-	var url = "https://api.nytimes.com/svc/books/v3/lists.json";
-	var book;
-	var books = [];
-	var arrayOfListOfBooks = ["Combined Print and E-Book Fiction",
-	"Combined Print and E-Book Nonfiction",
-	"Hardcover Fiction",
-	"Hardcover Nonfiction",
-	"Trade Fiction Paperback",
-	"Mass Market Paperback",
-	"Paperback Nonfiction",
-	"E-Book Fiction",
-	"E-Book Nonfiction",
-	"Hardcover Advice",
-	"Paperback Advice",
-	"Advice How-To and Miscellaneous",
-	"Chapter Books",
-	"Childrens Middle Grade",
-	"Childrens Middle Grade E-Book",
-	"Childrens Middle Grade Hardcover",
-	"Childrens Middle Grade Paperback",
-	"Paperback Books",
-	"Picture Books",
-	"Series Books",
-	"Young Adult",
-	"Young Adult E-Book",
-	"Young Adult Hardcover",
-	"Young Adult Paperback",
-	"Hardcover Graphic Books",
-	"Paperback Graphic Books",
-	"Manga",
-	"Combined Print Fiction",
-	"Combined Print Nonfiction",
-	"Animals",
-	"Business Books",
-	"Celebrities",
-	"Crime and Punishment",
-	"Culture",
-	"Education",
-	"Espionage",
-	"Expeditions Disasters and Adventures",
-	"Fashion Manners and Customs",
-	"Food and Fitness",
-	"Games and Activities",
-	"Hardcover Business Books",
-	"Health",
-	"Humor",
-	"Indigenous Americans",
-	"Relationships",
-	"Paperback Business Books",
-	"Family",
-	"Hardcover Political Books",
-	"Race and Civil Rights",
-	"Religion Spirituality and Faith",
-	"Science",
-	"Sports",
-	"Travel"];
+var url = "https://api.nytimes.com/svc/books/v3/lists.json";
+var book;
+var image;
+var books = [];
+var arrayOfListOfBooks = ["Combined Print and E-Book Fiction",
+"Combined Print and E-Book Nonfiction",
+"Hardcover Fiction",
+"Hardcover Nonfiction",
+"Trade Fiction Paperback",
+"Mass Market Paperback",
+"Paperback Nonfiction",
+"E-Book Fiction",
+"E-Book Nonfiction",
+"Hardcover Advice",
+"Paperback Advice",
+"Advice How-To and Miscellaneous",
+"Chapter Books",
+"Childrens Middle Grade",
+"Childrens Middle Grade E-Book",
+"Childrens Middle Grade Hardcover",
+"Childrens Middle Grade Paperback",
+"Paperback Books",
+"Picture Books",
+"Series Books",
+"Young Adult",
+"Young Adult E-Book",
+"Young Adult Hardcover",
+"Young Adult Paperback",
+"Hardcover Graphic Books",
+"Paperback Graphic Books",
+"Manga",
+"Combined Print Fiction",
+"Combined Print Nonfiction",
+"Animals",
+"Business Books",
+"Celebrities",
+"Crime and Punishment",
+"Culture",
+"Education",
+"Espionage",
+"Expeditions Disasters and Adventures",
+"Fashion Manners and Customs",
+"Food and Fitness",
+"Games and Activities",
+"Hardcover Business Books",
+"Health",
+"Humor",
+"Indigenous Americans",
+"Relationships",
+"Paperback Business Books",
+"Family",
+"Hardcover Political Books",
+"Race and Civil Rights",
+"Religion Spirituality and Faith",
+"Science",
+"Sports",
+"Travel"];
 
 	// var bookApp = angular.module("myBookApp",[]);
 
 	myApp.controller("BookCtrl", function($scope){
-			request();
+		request();
 		$scope.getBook = function(){
 			request();
 			$scope.title = book.title;
@@ -68,11 +69,39 @@
 			$scope.isbn = book.isbn;
 			$scope.desc = book.desc;
 			$scope.url = book.url;
+			console.log(book.url);
+			// requestServer(book.url);
+			// $scope.image = image;
+			$.ajax({
+
+				url: "/json/" + book.url,
+				type:'GET',
+				success: function(response) {
+					console.log(response);
+					$scope.image = response;
+				}
+				fail: function(response){
+					console.log("fail");
+				}               
+			});
+			
 
 			console.log("in bookController");
 		}
 
 	});
+
+	// function requestServer(amazonUrl) {
+	// 	$.ajax({
+
+	// 		url: "/json/" + amazonUrl,
+	// 		type:'GET',
+	// 		success: function(response) {
+	// 			console.log(response);
+	// 			image = response;
+	// 		}               
+	// 	});
+	// }
 
 	function request() {
 		$.ajax({
@@ -103,7 +132,7 @@
 		var desc;
 
 		var myJson = jQuery.parseJSON(JSON.stringify(json));
-			
+
 		myJson.results.forEach(function(item){
 			url = item.amazon_product_url;
 			console.log(url)
