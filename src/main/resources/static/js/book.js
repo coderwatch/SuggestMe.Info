a033e2d68ede3c416baffdd6ac03fab276b67f5a
Oscar Nevarez <ironbeaner@gmail.com>
2016-07-21 01:29:47 -0700

    getting close

--- src/main/resources/static/js/book.js
+++ src/main/resources/static/js/book.js
@@ -1,6 +1,7 @@
 
 var url = "https://api.nytimes.com/svc/books/v3/lists.json";
 var book;
+var results = [];
 var arrayOfListOfBooks = ["Combined Print and E-Book Fiction",
 "Combined Print and E-Book Nonfiction",
 "Hardcover Fiction",
@@ -58,42 +59,55 @@ var arrayOfListOfBooks = ["Combined Print and E-Book Fiction",
 // var bookApp = angular.module("myBookApp",[]);
 
 myApp.controller("BookCtrl", function($scope){
-		$scope.getBook = function(){
-			$scope.book = book;
-			console.log("in bookController" + $scope.book);
-		}
-		
-});
-$("#book-btn").click(function() {
-    $.ajax({
-        url: url+= '?' + $.param({
-  'api-key': "705fa0e1994d4a72a79cee53c93a05a6" ,
-  'list': arrayOfListOfBooks[Math.floor(Math.random()*arrayOfListOfBooks.length)]}),
-        type:'GET',
-        success: function(jsonResp) {
-        	console.log("sending get request!");
-        	parseJson(jsonResp);
-        }               
-    });
+	$scope.getBook = function(){
+		request();
+		$scope.title = book.title;
+		$scope.author = book.author;
+		$scope.genre = book.genre;
+		$scope.isbn = book.isbn;
+		$scope.desc = book.desc;
+		$scope.url = book.url;
+
+		console.log("in bookController");
+	}
+
 });
 
+function request() {
+	$.ajax({
+		url: url+= '?' + $.param({
+			'api-key': "705fa0e1994d4a72a79cee53c93a05a6" ,
+			'list': arrayOfListOfBooks[Math.floor(Math.random()*arrayOfListOfBooks.length)]}),
+		type:'GET',
+		success: function(jsonResp) {
+			console.log("sending get request!");
+			parseJson(jsonResp);
+			getRandomBook();
+		}               
+	});
+}
+
+function getRandomBook(){
+	var result = results[Math.floor(Math.random()*results.length)];
+	var resultBookDetails = result.book_details;
+	book = new Book(resultBookDetails.isbn,resultBookDetails.author,resultBookDetails.desc, resultBookDetails.title,resultBookDetails.genre,
+		result.amazon_product_url);
+	console.log("made a new Book " + book);
+}
 
 function parseJson(json){
 	var myJson = jQuery.parseJSON(JSON.stringify(json));
 	
 	myJson.results.forEach(function(item){
-		item.book_details.forEach(function(item){
-			
-			book = new Book(item.primary_isbn13, item.author, item.description, item.title, item.list_name);
-			console.log("created book: "+book.author);
-		});
+			results.push(item);
 	});	
 }
 
-function Book(isbn, author, desc, title, genre){
+function Book(isbn, author, desc, title, genre, url){
 	this.isbn = isbn;
 	this.author = author;
 	this.desc = desc;
 	this.title = title;
 	this.genre = genre;
+	this.url = url;
 }
