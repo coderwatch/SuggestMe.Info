bf2620b55ccc93bbd920b24356a5c28436dddb0e
Josue Ruiz <Josue Ruiz>
2016-07-21 01:30:20 -0700

    added changes to jsouptest

--- src/test/java/edu/csupomona/cs480/Jsouptest.java
+++ src/test/java/edu/csupomona/cs480/Jsouptest.java
@@ -5,15 +5,15 @@ import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
+//import org.jsoup.select.Elements;
 
 public class Jsouptest {
 	
 	public static void main(String[] args) throws IOException{
 		String URL = "https://www.amazon.com/Pride-Prejudice-Jane-Austen/dp/1503290565/ref=sr_1_2?ie=UTF8&qid=1469082902&sr=8-2&keywords=pride+and+prejudice";
 		Document doc = Jsoup.connect(URL).get();
-		Elements elements =doc.select("div.maintain-height img");
-	    System.out.println(elements.attr("src"));
+		//Elements elements =doc.select("span[class=price final-price our fksk-our]");
+	    System.out.println(doc.select("span[class=a-size-large]").text());
 	}
 
 }
